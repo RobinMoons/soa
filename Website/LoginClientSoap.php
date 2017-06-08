c88df0c65095e44f7b031b66cb333b7e0a9bad82
DESKTOP-8IABVA6\kobe_ <kobe_bamps@hotmail.com>
2017-06-08 21:47:00 +0200

    aangepast

--- Website/LoginClientSoap.php
+++ Website/LoginClientSoap.php
@@ -1,15 +1,18 @@
 
 <?php
-
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 if (isset($_POST['gebruikersnaam'])) {
     $client = new SoapClient("http://localhost/SOAproject/UserService_PHP/UserManagerService.php?wsdl", 
             array('trace' => 1, 'cache_wsdl' => WSDL_CACHE_NONE));
 
     $object = $client->login($_POST['gebruikersnaam'], $_POST['wachtwoord']);
 
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Origin: *');
-    print(json_encode(array("gebruiker" => $object)));
+    if ($object == NULL) {
+    	print(json_encode(["mislukt"=>"gebruikersnaam of paswoord incorrect"]);
+    } else {    
+    	print(json_encode(array("gebruiker" => $object)));
+	}
 }
 ?>
        
