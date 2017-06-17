34df473038c660f1db551db035a80b98868c6666
DESKTOP-8IABVA6\kobe_ <kobe_bamps@hotmail.com>
2017-06-17 14:53:33 +0200

    alle services opgehaald

--- EnergieBronBepaler/EnergieBronBepalerRest.php
+++ EnergieBronBepaler/EnergieBronBepalerRest.php
@@ -14,8 +14,34 @@ if (!isset($_GET['methode']) && !isset($_POST['methode'])) {
     $client = new SoapClient("http://localhost/SOAproject/EnergieBronBepaler/GasLeverancierService.php?wsdl", 
             array('trace' => 1, 'cache_wsdl' => WSDL_CACHE_NONE));
 
-    $leverancier = $client->getGasleverancier($_GET['leverancier']);
-    echo($leverancier[0]->leverancier);
+    $gasleverancier = $client->getGasleverancier($_GET['leverancier']);
+    $gasPrijzen = $client->getGasprijzen($gasleverancier->id);
+
+    $urlenergie = "http://usermanager-167313.appspot.com/getData?&key=". "5717271485874176" . "&distributor=" . $gasleverancier->leverancier;
+    $ch = curl_init($urlenergie);
+	curl_setopt_array($ch, array( 
+	    CURLOPT_FOLLOWLOCATION => true,
+	    CURLOPT_HEADER => false,
+	    CURLOPT_RETURNTRANSFER => true
+	));
+
+    $energieleverancier = json_decode(curl_exec($ch));
+
+    $urlweer = "http://api.openweathermap.org/data/2.5/forecast?id=" . "2795648" . "&units=metric&APPID=a4a530758bce79a5b8ef70c4b2a2a71b&mode=json";
+    $ch = curl_init($urlweer);
+	curl_setopt_array($ch, array( 
+	    CURLOPT_FOLLOWLOCATION => true,
+	    CURLOPT_HEADER => false,
+	    CURLOPT_RETURNTRANSFER => true
+	));
+
+	$openw =  json_decode(curl_exec($ch));
+
+	//echo curl_exec($ch);
+	for ($i=0; $i < 10; $i++) { 
+		echo json_encode($openw->list[$i]->clouds->all);
+	}
+	
 
 }else if (isset($_POST['methode'])) {
 
