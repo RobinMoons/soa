c54bfdae5eaa182818bf4a86a90fec7d2ef16f10
DESKTOP-8IABVA6\kobe_ <kobe_bamps@hotmail.com>
2017-06-18 12:26:51 +0200

    rest documentatie

--- UserService_PHP/usermanagerREST.php
+++ UserService_PHP/usermanagerREST.php
@@ -12,6 +12,15 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
 if (!isset($_GET['methode']) && !isset($_POST['methode'])) {
     die("No method selected");
+}elseif ($_GET['methode'] == 'info') {
+    $info = new \stdClass();
+    $info->GET->methode->dataGebruiker->beschrijving = "Deze methode haalt de gevraagde gebruiker op uit de mysql database. Als de API token niet correct is, kan er ook geen gebruiker opgehaald worden";
+    $info->GET->methode->dataGebruiker->parameters->data = "De JWT token";
+    $info->POST->methode->nieuweUser->beschrijving = "Hiermee wordt er een nieuwe user toegevoegd aan de database";
+    $info->POST->methode->nieuweUser->parameters->emailadres = "emailadres";
+    $info->POST->methode->nieuweUser->parameters->gebruikersnaam ="gebruikersnaam";
+    $info->POST->methode->nieuweUser->parameters->wachtwoord = "wachtwoord";
+    print json_encode($info);
     
 }else if (isset($_GET['methode'])) {
     switch ($_GET['methode']) {
